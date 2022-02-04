dd85bd6ee5c50cf79bd148fbd0f89bff77486145
vadim <vadik5719@yandex.ru>
2022-02-04 21:15:54 +0400

    Task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Максимальное число из трех
+Console.Write("Введите первое число - ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("max = " + max);
