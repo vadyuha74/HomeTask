eb90eba30f5902111c44ddf44f7abd624d350a72
vadim <vadik5719@yandex.ru>
2022-02-04 22:12:55 +0400

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные чила от 1 до N
+int N = new Random().Next(1, 100);
+Console.WriteLine("Случайное число - " + N);
+if (N%2 != 0) N = N - 1;
+while (N > 1)
+{
+    Console.WriteLine(N);
+    N = N - 2;
+}
