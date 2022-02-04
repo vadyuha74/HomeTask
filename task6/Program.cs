9e616d031bfb37480b0cbc63586839e72c1ed525
vadim <vadik5719@yandex.ru>
2022-02-04 21:42:55 +0400

    Task6

---
+++ task6/Program.cs
@@ -0,0 +1,5 @@
+﻿// Выяснить, являеться число четным
+int a = new Random().Next(1, 100);
+Console.WriteLine(a);
+if (a%2 == 0) Console.WriteLine("число четное");
+else Console.WriteLine("число нечетное");
