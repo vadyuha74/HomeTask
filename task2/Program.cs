5ca0338a23e2248181e26271cbe69793a4b1fef2
vadim <vadik5719@yandex.ru>
2022-02-04 20:50:06 +0400

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показывает наибольшее и наименьшее число из двух
+int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+if (a == b) Console.WriteLine("Числа равны");
+else
+{
+    if (b > a)
+    {
+        Console.WriteLine("наибольшее число - " + b);
+        Console.WriteLine("наиьеньшее число - " + a);
+    }
+    else
+    {
+    Console.WriteLine("наибольшее число - " + a);
+    Console.WriteLine("наиьеньшее число - " + b);
+    }
+}
