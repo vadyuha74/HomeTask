cf46ba9e3c56f5f273b60bb407003292bf709a03
vadim <vadik5719@yandex.ru>
2022-02-06 06:37:16 +0400

    fix task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -6,7 +6,6 @@ int b = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число - ");
 int c = int.Parse(Console.ReadLine());
 int max = a;
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 Console.WriteLine("max = " + max);
