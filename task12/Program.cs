fa07f9d6bd6fb901b74d33fc54569e339d9b00ce
vadim <vadik5719@yandex.ru>
2022-02-08 09:22:49 +0400

    Task14

---
+++ task12/Program.cs
@@ -0,0 +1,24 @@
+﻿// Удалить вторую цифру трёхзначного числа с защитой от неправильного ввода
+int DelSecondDig(int num)
+{
+    int dig = (num / 100) * 10 + num % 10;
+    return dig;
+}
+// Ввод и проверка числа
+int InputExamNumber()
+{
+    while (true)
+    {
+        Console.Write("Введите трехзначное число: ");
+        string text = Console.ReadLine();
+        if (text.Length == 3 & int.TryParse(text, out int number))
+        {
+        Console.WriteLine("Вы ввели число {0}", number);
+        return number;
+        break;
+        }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+int a = InputExamNumber();
+Console.WriteLine($"Число без второй цифры - {DelSecondDig(a)}");
---
+++ task14/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+int InputExamNumber()
+{
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+        Console.WriteLine("Вы ввели число {0}", number);
+        return number;
+        break;
+        }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+int a = InputExamNumber();
+if (a < 100) Console.WriteLine("Третьей цифры нет");
+else
+{
+     while (a >= 1000)
+    {
+        a = a / 10;
+    }
+a = a % 10;
+Console.WriteLine($"третья цифра - {a}");
+}
