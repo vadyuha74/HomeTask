0670286b4a7e6858236ea2512ba9f23a87e86c98
vadim <vadik5719@yandex.ru>
2022-02-17 10:50:20 +0400

    Решение task32

---
+++ task32/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+void FillArray(int[] mass)
+{
+    int length = mass.Length;
+    int index = 0;
+    while (index < length)
+    {
+        mass[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+void OutputArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int[] array = new int[8];
+FillArray(array);
+OutputArray(array);
