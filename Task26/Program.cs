0f773239433e8e4d4ae54601bc7d7295cdee55fd
vadim <vadik5719@yandex.ru>
2022-02-20 22:41:17 +0400

    Решение task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,6 +1,41 @@
 ﻿// Возведите число А в натуральную степень B используя цикл
-Console.WriteLine("Hello, World!");
-for (int i = 0; i < b; i++)
+int InputExamNumber() // Ввод и проверка числа
 {
-    a *= a;
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            Console.WriteLine("Вы ввели число " + number);
+            return number;
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
 }
+double Exponent(int a, int b) // возведенеие в степень
+{
+    double c = 1;
+    if (b > 0)
+    {
+        for (int i = 0; i < b; i++)
+        {
+            c *= a;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < -b; i++)
+        {
+            c /= a;
+        }
+    }
+    return c;
+}
+int a, b;
+Console.WriteLine("Возведение число А в натуральную степень B");
+Console.WriteLine("Введите число А");
+a = InputExamNumber();
+Console.WriteLine("Введите число B");
+b = InputExamNumber();
+Console.WriteLine("результат равен: " + Exponent(a, b));
