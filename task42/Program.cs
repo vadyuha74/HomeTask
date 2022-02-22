d7e6b1bafce1259e3e573aa80543b356a48093be
vadim <vadik5719@yandex.ru>
2022-02-22 18:09:46 +0400

    решение task42

---
+++ task42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+int count = 0;
+Console.WriteLine("Введите целое число, если ввод закончен введите n");
+while (true)
+{
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        if (number >0) count++;
+    } 
+    else 
+    {
+        if (text == "n") break;
+        else Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+Console.WriteLine("Количество чисел больше нуля: {0}", count);
