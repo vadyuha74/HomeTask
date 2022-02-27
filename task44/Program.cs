748c0ba55f31b61c24f775da60921492417ec4c5
vadim <vadik5719@yandex.ru>
2022-02-27 20:45:02 +0400

    решение task46

---
+++ task44/Program.cs
@@ -0,0 +1,62 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+int InputExamNumber() // Ввод и проверка числа
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            Console.WriteLine("Вы ввели число " + number);
+            return number;
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+bool ChoiceUsers() // выбор пользователя
+{
+    while (true)
+    {
+        Console.WriteLine("Если согласны введите Y, в обратном случае нажмите N");
+        string text = Console.ReadLine();
+        if (text == "Y" || text =="y")
+        {
+            return true;
+            break;
+        }
+        if (text == "N" || text =="n")
+        {
+            return false;
+            break;
+        }
+        else Console.WriteLine("Не удалось распознать ответ, попробуйте еще раз.");
+    }
+}
+int[] InpArrayHowString()
+{
+    
+    Console.WriteLine("Введите числа через запятую, в виде 1,2,3,4");
+    string text = Console.ReadLine().Split(",");
+    for (int i = 0; i < text.length; i++)
+    {
+        
+    }
+}
+int[] arr = new int[4];
+Console.WriteLine("Находим точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Хотите задать k1, b1, k2, b2? В другом случае точки будут случайными");
+if (ChoiceUsers())
+{
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine($"Введите k{i}");
+        arr[i]=InputExamNumber();
+        Console.WriteLine($"Введите b{i}");
+        arr[i+1]=InputExamNumber();
+    }
+
+}
+else Console.WriteLine("Досвидания");
+/*double y = (k2 * b1 / k1 + b2) / (1 - k2 / k1);
+double x = (y - b1) / k1;*/
---
+++ task46/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу масштабирования фигуры
+
+int[,] AddPointToArray() //Ввод точек и преобразование в массив
+{
+    Console.WriteLine("Введите точки фигуры в виде (0,0) (2,0) (2,2) (0,2)");
+    string[] text = Console.ReadLine().Replace(")", "").Replace("(", "").Split(" ");
+    int[,] array = new int[text.Length, 2];
+    for (int i = 0; i < text.Length; i++)
+    {
+        string[] point = text[i].Split(",");
+        array[i, 0] = Convert.ToInt32(point[0]);
+        array[i, 1] = Convert.ToInt32(point[1]);
+    }
+    return array;
+}
+
+void PrintPoints(int[,] matr) // Вывод точек
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+            Console.Write($"({matr[i, 0]},{matr[i, 1]})");
+    }
+    Console.WriteLine();
+}
+
+int[,] ScaleArray(int[,] matr, int k) // Умножение массива на число
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] *= k;
+        }
+    }
+    return matr;
+}
+
+Console.WriteLine("Программа масштабирования фигуры");
+int[,] arrayPoint = AddPointToArray();
+Console.Write("Введите коэффициэнт масштабирования:");
+int scale = int.Parse(Console.ReadLine());
+arrayPoint = ScaleArray(arrayPoint, scale);
+PrintPoints(arrayPoint);
