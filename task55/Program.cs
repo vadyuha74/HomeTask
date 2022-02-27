12c2f0aa84fc50adf5eb84b625df9a32a68064e3
vadim <vadik5719@yandex.ru>
2022-02-27 22:48:26 +0400

    решение task55

---
+++ task55/Program.cs
@@ -0,0 +1,59 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+int InputExamNumber() // ввод и проверка числа
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            return number;
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+
+void PrintArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void MeanArithColumn(int [,] matr)
+{
+    double summ=0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            summ += matr[j, i];
+        }
+        Console.WriteLine($"Среднее арифмитическое {i+1} солбца равно {summ/matr.GetLength(0)}");
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int m = InputExamNumber();
+Console.WriteLine("Введите количество столбцов");
+int n = InputExamNumber();
+int [,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+MeanArithColumn(array);
