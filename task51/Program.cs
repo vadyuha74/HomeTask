507250da25d84fdaab59f65bd482fa6c6b1c6915
vadim <vadik5719@yandex.ru>
2022-02-27 21:15:14 +0400

    решение task51

---
+++ task51/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задать двумерный массив следующим правилом: A(m,n) = m+n.
+int InputExamNumber() // ввод и проверка числа
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            return number;
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
+void PrintArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i+j;
+        }
+    }
+}
+Console.WriteLine("Введите количество строк");
+int m = InputExamNumber();
+Console.WriteLine("Введите количество столбцов");
+int n = InputExamNumber();
+int [,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
